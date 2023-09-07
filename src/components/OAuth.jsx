16af15874f8a9aaf1b5d602a13348df48e6a27c4
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-09-07 16:51:48 +0100

    complete th oauth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
-import { FcGoogle } from 'react-icons/fc'
+import React from 'react';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import { getDoc } from 'firebase/firestore';
+import { FcGoogle } from 'react-icons/fc';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // check if user exists in db
+      const userDocRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(userDocRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(userDocRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate('/');
+    } catch (error) {
+      toast.error('Could not sign in with Google');
+      console.log(error);
+    }
+  }
+
   return (
-    <button className='flex items-center 
-    justify-center 
-    w-full bg-red-700
-    text-white px-7 py-3 
-    uppercase text-sm font-medium hover:bg-red-800
-     active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded '>
-      <FcGoogle className='text-2xl bg-white rounded-full mr-2'/>
-      Continue with 
-      Google
+    <button
+      type='button'
+      onClick={onGoogleClick}
+      className='flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded'
+    >
+      <FcGoogle className='text-2xl bg-white rounded-full mr-2' />
+      Continue with Google
     </button>
-  )
+  );
 }
