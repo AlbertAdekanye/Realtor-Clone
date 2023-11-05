1d233cf996344146e444153625fcfb2e3e408b8e
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-11-05 21:16:39 +0100

    added my listing section to the profile page and create the listingItems to the components

---
+++ src/components/ListingItems.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ListingItems(listing, id) {
+  return (
+    <div>{listing.name}</div>
+  )
+}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import { useState } from 'react';
-import {getAuth, updateProfile} from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import {db} from '../firebase';
-import { doc, updateDoc } from 'firebase/firestore';
-import { FcHome } from 'react-icons/fc';
-import { Link } from 'react-router-dom';
+import { getAuth, updateProfile } from "firebase/auth";
+import {
+  collection,
+  // deleteDoc,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { FcHome } from "react-icons/fc";
+import { useEffect } from "react";
+import ListingItems from "../components/ListingItems";
+// import ListingItem from "../components/ListingItem";
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
   const [changeDetail, setChangeDetail] = useState(false);
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -46,6 +58,26 @@ export default function Profile() {
       toast.error("could not update the profile details")
     }
   }
+  useEffect(()=>{
+    async function fetchUserListings(){
+      const listingRef = collection(db, 'listings');
+      const q = query(listingRef, 
+        where('userRef', '==', auth.currentUser.uid), 
+        orderBy('timestamp', 'desc')
+      );
+      const querySnapshot = await getDocs(q);
+      let listings = [];
+      querySnapshot.forEach((doc)=>{
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+      setLoading(false);
+    }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -87,6 +119,24 @@ export default function Profile() {
           </button>
         </div>
       </section>
+      <div className="max-w-6xl px-3 mt-6 px-auto">
+        {!loading && listings.length > 0 && (
+          <>
+           <h2 className="text-2xl text-center font-semibold">
+              My Listings
+           </h2>
+           <ul className="">
+            {listings.map((listing)=>(
+              <ListingItems 
+                key={listing.id}
+                id={listing.id}
+                listing={listing.data}
+              />
+            ))}
+           </ul>
+          </>
+        )}
+      </div>
     </>
   )
 }
