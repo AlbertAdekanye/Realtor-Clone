b1c66de157fbae570cade18b77b4e89f36ff1ee6
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-11-11 20:53:33 +0100

    add the listing information to the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -13,7 +13,7 @@ import SwiperCore, {
 } from "swiper";
 import "swiper/css/bundle";
 import { getAuth } from "firebase/auth";
-import {FaShare} from 'react-icons/fa'
+import {FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair} from 'react-icons/fa'
 
 export default function Listing() {
   const auth = getAuth();
@@ -75,6 +75,73 @@ export default function Listing() {
             Link Copied
         </p>
       )}
+
+      <div className="flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
+        {/* info */}
+        <div className="w-full h-[200px] lg-[400px]">
+          <p className="text-2xl font-bold mb-3 text-blue-900">
+            {listing.name} - $ {""} {listing.offer ? listing.discountedPrice.toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",") :  listing.regularPrice}.toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            {listing.type === "rent" ? "/month" : ""}
+          </p>
+          {/* Address */}
+          <p className="flex items-center justify-center mt-6 mb-3 font-semibold ">
+            <FaMapMarkerAlt className="text-green-700 mr-1"/>
+            {listing.address}
+          </p>
+          <div className="flex justify-start items-center space-x-4 w-[75%]">
+            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+              {listing.type === "rent" ? "For Rent" : "For Sale"}
+            </p>
+            <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md">
+              {listing.offer && (
+                <p className="">
+                  ${+listing.regularPrice - +listing.discountedPrice} 
+                  discount
+                </p>
+              )}
+            </p>
+          </div>
+          <p className="mt-3 mb-3">
+            <span className="font-semibold">
+              Description -
+            </span>
+            {listing.description}
+          </p>
+          <div className="">
+            <ul className="flex items-center space-x-2 lg:space-x-10 text-sm font-semibold">
+              {/* bed */}
+              <li className="flex items-center whitespace-nowrap">
+                <FaBed className="text-lg mr-1"/>
+                <span className="font-semibold">Beds - </span>
+                {+listing.beds > 1 ? `${listing.bedrooms} Beds` : "1 Bed" }
+              </li>      
+              {/* bath */}
+              <li className="flex items-center whitespace-nowrap">
+                <FaBath className="text-lg mr-1"/>
+                <span className="font-semibold">Baths - </span>
+                {+listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath" }
+              </li>      
+              
+              {/* parking */}
+              <li className="flex items-center whitespace-nowrap">
+                <FaParking className="text-lg mr-1"/>
+                {listing.parking ? "Parking Spot" : "No parking" }
+              </li>               
+              
+              {/* furniture */}
+              <li className="flex items-center whitespace-nowrap">
+                <FaChair className="text-lg mr-1"/>
+                {listing.furnished ? "Furnished" : "Not furnished" }
+              </li>
+            </ul>
+          </div>
+        </div>
+        {/* map */}
+        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+      </div>
     </main>
+    // “Do the best you can until you know better. Then when you know better, do better.” 
   
 }
