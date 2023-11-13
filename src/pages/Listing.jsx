517630d63411f86e0b4bac55dd26c2aa38ddf720
Albert Adekanye <74176768+AlbertAdekanye@users.noreply.github.com>
2023-11-13 22:47:39 +0100

    create the landlord contact component

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -14,6 +14,7 @@ import SwiperCore, {
 import "swiper/css/bundle";
 import { getAuth } from "firebase/auth";
 import {FaShare, FaMapMarkerAlt, FaBed, FaBath, FaParking, FaChair} from 'react-icons/fa'
+import Contact from "../components/Contact";
 
 export default function Listing() {
   const auth = getAuth();
@@ -21,6 +22,7 @@ export default function Listing() {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [shareLinkCopied, setShareLinkCopied] = useState(false);
+  const [contactLandlord, setContactLandlord] = useState(false);
   
   SwiperCore.use([Autoplay, Navigation, Pagination]);
   useEffect(() => {
@@ -37,7 +39,7 @@ export default function Listing() {
   if (loading) {
     return <Spinner />;
   }
-  return <main>
+  return (<main>
       <Swiper
         slidesPerView={1}
         navigation
@@ -78,7 +80,7 @@ export default function Listing() {
 
       <div className="flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
         {/* info */}
-        <div className="w-full h-[200px] lg-[400px]">
+        <div className="w-full">
           <p className="text-2xl font-bold mb-3 text-blue-900">
             {listing.name} - $ {""} {listing.offer ? listing.discountedPrice.toString()
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ",") :  listing.regularPrice}.toString()
@@ -110,7 +112,7 @@ export default function Listing() {
             {listing.description}
           </p>
           <div className="">
-            <ul className="flex items-center space-x-2 lg:space-x-10 text-sm font-semibold">
+            <ul className="flex items-center space-x-2 lg:space-x-10 text-sm font-semibold mb-6">
               {/* bed */}
               <li className="flex items-center whitespace-nowrap">
                 <FaBed className="text-lg mr-1"/>
@@ -137,11 +139,32 @@ export default function Listing() {
               </li>
             </ul>
           </div>
+          {/* contact */}
+          {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
+            <div className="mt-6">
+              <button className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition duration-150 ease-in-out" 
+              onClick={() => setContactLandlord(true)}>
+                Contact Landlord
+              </button>
+            </div>
+          )}
+          {contactLandlord && (
+            <Contact  
+            userRef={listing.userRef} listing={listing}/>
+          )}
         </div>
         {/* map */}
-        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden">
+          {/* <iframee
+            className="w-full h-full"
+            src={`https://maps.google.com/maps?q=${listing.address}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
+            loading="lazy"
+          ></iframee> */}
+        </div>
       </div>
     </main>
-    // “Do the best you can until you know better. Then when you know better, do better.” 
   
+  );
 }
+
+// “Do the best you can until you know better. Then when you know better, do better.” 
